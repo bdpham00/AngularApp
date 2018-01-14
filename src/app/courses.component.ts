b090cc91f4d2cafce52a53656f2f97e0c979541c
BAO-PC\Bao <bdpham00@gmail.com>
2018-01-13 16:11:31 -0800

    event handlers

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -8,7 +8,15 @@ import { Component } from '@angular/core'
                     <li *ngFor="let course of courses">
                         {{course}}
                     </li>
-               </ul>`
+               </ul>
+               <div>
+               <div (click)="onDivClicked()">
+               <button (click)="onSave($event)">Save</button>
+                </div>
+               </div>
+               
+               <input (keyup.enter)="onKeyUp()" />
+               `
 })
 export class CoursesComponent {
     title = 'List of Courses';
@@ -17,6 +25,23 @@ export class CoursesComponent {
     constructor() {
         let service = new CoursesService(); 
         this.courses = service.getCourses(); 
+    }
+
+    onKeyUp() {
+        // if ($event.keyCode === 13) {
+        //     console.log("ENTER was pressed"); 
+        // }
+        console.log("ENTER pressed"); 
+    }
+
+    onSave($event) {
+        //Prevents Event bubbling
+        $event.stopPropagation(); 
+        console.log("Button clicked!", $event); 
+    }
+
+    onDivClicked() {
+        console.log("Div clicked"); 
     }
     
     getTitle() {
