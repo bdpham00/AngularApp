15edf9d62043facbc483fbe08aedc9a4fcd3b054
BAO-PC\Bao <bdpham00@gmail.com>
2018-02-18 03:51:23 -0800

    shouldBeUnique is now asynchronouse

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -1,6 +1,7 @@
 import { AbstractControl } from "@angular/forms";
 import { ValidationErrors } from "@angular/forms/src/directives/validators";
 import { setTimeout } from "timers";
+import { reject } from "q";
 
 export class UsernameValidators {
     static cannotContainSpace(control: AbstractControl): ValidationErrors {
@@ -10,17 +11,17 @@ export class UsernameValidators {
         return null; 
     }
 
-    static shouldBeUnique(control: AbstractControl): ValidationErrors | null {
+    static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors|null> {
         //Asynchronous Operation: Calling the server may take a few seconds. User cannot interact with the browser
         //during this time. 
 
-        setTimeout(() => {
-            console.log('ok'); 
-        }, 2000); 
-
-        if((control.value as string) === "bao")
-            return { shouldBeUnique: true }
-
-        return null;  
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if((control.value as string) === "bao")
+                    resolve({ shouldBeUnique: true }); 
+    
+                resolve(null);   
+            }, 2000); 
+        })
     }
 }
