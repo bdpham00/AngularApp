f93ccaa5276aab4b998bdbb8f6cab472af75f0b9
bdpham <bdpha@DESKTOP-M4UUU5N>
2018-02-18 03:02:50 -0800

    Added Validators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SignupFormComponent } from './signup-form/signup-form.component';
 import { TitleCasePipe } from './title-case.pipe';
 import { FavoritesComponent } from './favorites/favorites.component';
 import { AuthorsComponent } from './authors.component';
@@ -6,7 +7,7 @@ import { CoursesComponent } from './courses.component';
 import { CoursesService } from './courses.service';
 
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
 
 
@@ -15,21 +16,25 @@ import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
 import { ZippyComponent } from './zippy/zippy.component';
 import { CourseComponent } from './course/course.component';
-import { SignupFormComponent } from './signup-form/signup-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent, 
-    SignupFormComponent,
     AuthorsComponent,
     CoursesComponent,
     FavoritesComponent, 
-    TitleCasePipe, PanelComponent, LikeComponent, ZippyComponent, CourseComponent
+    TitleCasePipe, 
+    PanelComponent, 
+    LikeComponent, 
+    ZippyComponent, 
+    CourseComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule, 
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     AuthorsService,
--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'; 
 
 @Component({
   selector: 'signup-form',
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup-form.component.css']
 })
 export class SignupFormComponent {
+  form = new FormGroup({
+    'username': new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    'password': new FormControl('', Validators.required)
+  }); 
+  
+  get username() {
+    let model = this.form.get('username')
+    console.log(model);
+    return model; 
+  }
 }
